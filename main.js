4bcb960d6ec9af90d596d869d73158fb77ffb80a
D2B <suporte@d2bonline.com.br>
2021-10-28 14:53:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+/* TESTIMONIALS SWIPER */
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* SCROLL REVEAL */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .text, #home .image, #about .image, #about .text, #occupation header, #occupation .card,#team header, #team .team, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links
+`,
+  { interval: 100 }
+)
