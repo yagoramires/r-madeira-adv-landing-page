41f9e3ad5644e2db56bdd617f10b08dfdf7f3abd
Yago Ramires <77733200+yagoramires@users.noreply.github.com>
2021-11-04 14:46:01 -0300

    Final Changes
    Finished Web Version CSS

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* TESTIMONIALS SWIPER */
 
@@ -34,7 +34,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* SCROLL REVEAL */
@@ -48,7 +54,50 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
   `
-  #home .text, #home .image, #about .image, #about .text, #occupation header, #occupation .card,#team header, #team .team, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links
+  #home .text, #home .image, #about .image, #about .text, #occupation header, #occupation .card,#team header, #team .team, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+/* BACK TO TOP BUTTON */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Active menu by page section */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* WHEN SCROLL */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
